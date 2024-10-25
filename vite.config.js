3540f4f5aa5136424db3b3e7df343c21cf739139
SonyVansha <sonyvansha@gmail.com>
2024-10-26 03:59:40 +0700

    fix: fixing path dist

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/test-deploy-react/",
+  base: ".",
   server: {
     host: '0.0.0.0',
-    port: 3000,
+    port: 80,
   }
 })
